efcc72d2fe3a0c780aa7ea0b849aeabcf68491b9
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-14 20:14:13 +0530

    __str__ fun created in class

--- cosmecure/users/models.py
+++ cosmecure/users/models.py
@@ -8,4 +8,6 @@ class users(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.firstname} ({self.lastname})"
     
