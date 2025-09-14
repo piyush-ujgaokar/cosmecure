5e0b4e3ee5bcddfb80e4df80e00d056e97ff4349
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-14 21:02:45 +0530

    Successful msg displayed and redirect to login

--- cosmecure/users/views.py
+++ cosmecure/users/views.py
@@ -24,10 +24,13 @@ def signup_view(request):
             messages.error(request, "Email already registered.")
             return redirect('signup')
         hashed_password = make_password(password)
-        users.objects.create(
+        users.objects.create(#new instance of user,save it directly to db
             firstname=firstname,
             lastname=lastname,
             email=email,
             phone=phone,
             password=hashed_password
         )
+        messages.success(request, "Signup successful! Please log in.")
+        return redirect('login')
+    
