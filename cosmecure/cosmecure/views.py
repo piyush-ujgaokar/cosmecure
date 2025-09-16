e82b83ea80ee41f2c8b787f759e0cc027a32cb74
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-16 23:06:06 +0530

    View added for quiz

--- cosmecure/cosmecure/views.py
+++ cosmecure/cosmecure/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 def home_view(request):
     user_id = request.session.get('user_id')
-    return render(request, 'home/home.html', {'user_id': user_id})
+    return render(request, 'home/home.html')
 
 def about_view(request):
     return render(request, 'aboutus/about.html')
+
+def quiz_view(request):
+    return render(request, 'quiz/quiz.html')
