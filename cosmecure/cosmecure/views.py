601cc0d552c9e5e49282947d413a32a3e7ead710
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-16 18:48:51 +0530

    View created for about.html

--- cosmecure/cosmecure/views.py
+++ cosmecure/cosmecure/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home/home.html')
+
+def about_view(request):
+    return render(request, 'aboutus/about.html')
