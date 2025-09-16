453a6e5b865411a0d1bd4c9d9507d0b9dc6ef04b
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-16 19:31:52 +0530

    Post method and csrf token added

--- cosmecure/cosmecure/views.py
+++ cosmecure/cosmecure/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    if request.method == 'POST':
+        return render(request, 'home/home.html')
 
 def about_view(request):
     return render(request, 'aboutus/about.html')
