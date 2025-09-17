4a93ef1aa077234e2a68090aedea9fa2e3937bdf
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-17 15:52:33 +0530

    buy_now view is created

--- cosmecure/products/views.py
+++ cosmecure/products/views.py
@@ -7,3 +7,15 @@ def product_list(request, skin_type):
     products = Product.objects.filter(skin_type__iexact=skin_type)
     context = {'products': products, 'skin_type': skin_type}
     return render(request, 'skincare/normal.html', context)
+
+def buy_now(request, product_id):
+    
+    if request.method == 'POST':
+        product = get_object_or_404(Product, id=product_id)
+        
+        request.session['product_id'] = product.id
+        
+        
+        return redirect('buy_now_page')
+    
+    return redirect('product_list', skin_type='normal')
