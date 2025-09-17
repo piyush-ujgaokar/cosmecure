26d67b65252c3b1f5583c3ebc81cb871475ef7f5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-17 11:47:34 +0530

    Product name and price taken from session

--- cosmecure/orders/views.py
+++ cosmecure/orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from models import Order
 # Create your views here.
+def process_order(request):
+    if 'product_name' in request.session and 'product_price' in request.session:
+        product_name = request.session.get('product_name')
+        product_price = request.session.get('product_price')
