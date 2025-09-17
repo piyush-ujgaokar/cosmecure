945e38293a220cb8432a66d848a8811364bb6f51
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-17 20:48:22 +0530

    template_map created to store skin type and render pgs

--- cosmecure/products/views.py
+++ cosmecure/products/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
 from django.http import Http404
+
+def skin_type_products(request, skin_type):
+    # Define a map to link skin types to specific templates
+    template_map = {
+        'oily': 'products/oily_products.html',
+        'dry': 'products/dry_products.html',
+        'combination': 'products/combination_products.html',
+        'normal': 'products/normal_products.html',
+    }
