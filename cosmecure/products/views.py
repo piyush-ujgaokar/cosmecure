f1af12719e620e2f6f41561ef0f9f2ba88795a3d
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-17 15:32:01 +0530

    normal_skincare view created

--- cosmecure/products/views.py
+++ cosmecure/products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def normal_skincare(request):
+    return render(request, 'skincare/normal.html')
