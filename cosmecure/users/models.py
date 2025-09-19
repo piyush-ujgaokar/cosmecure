4b6d1890e80cbc314f8f03c0b2ec19d8017a824e
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-19 23:41:01 +0530

    class users updated and phone imported

--- cosmecure/users/models.py
+++ cosmecure/users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class users(models.Model):
-    
+class users(AbstractUser):
+    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
 
     def __str__(self):
         return f"{self.firstname} ({self.lastname})"
