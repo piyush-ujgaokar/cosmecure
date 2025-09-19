98a903adc824d7a4815b14b00a0e477ad3bfed24
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-19 23:39:54 +0530

    AbstractUser imported

--- cosmecure/users/models.py
+++ cosmecure/users/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class users(models.Model):
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return f"{self.firstname} ({self.lastname})"
