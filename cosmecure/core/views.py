03457d2c7df30fe37daafa5d17c2013e858bd8af
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-20 21:19:15 +0530

    core urls included

--- cosmecure/core/views.py
+++ cosmecure/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home/home.html')
+
+def about_view(request):
+    return render(request, 'aboutus/about.html')
+
