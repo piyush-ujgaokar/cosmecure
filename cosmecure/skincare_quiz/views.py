6df2558a5d1c766198d8e433ebe0fadf2b3379e3
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-24 20:36:40 +0530

    skincare_quiz view created

--- cosmecure/skincare_quiz/views.py
+++ cosmecure/skincare_quiz/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
+def skincare_quiz(request):
+    return render(request, 'quiz/skincare_Quiz.html')
