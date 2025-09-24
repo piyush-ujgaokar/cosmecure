c34d8d5c35f8ae1cdb02e7bf6329a9258b1d1e89
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-24 20:42:36 +0530

    path and login_required imported

--- cosmecure/skincare_quiz/urls.py
+++ cosmecure/skincare_quiz/urls.py
@@ -1,1 +1,9 @@
 from . import views
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+
+
+urlpatterns = [
+path('skincare_quiz/', views.skincare_quiz, name='skincare_quiz'),
+]
