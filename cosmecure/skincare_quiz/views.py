094aec4cedcaad98d8be6521eca06634390d0ec4
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-25 10:11:28 +0530

    template_map created

--- cosmecure/skincare_quiz/views.py
+++ cosmecure/skincare_quiz/views.py
@@ -7,3 +7,10 @@ from django.http import Http404
 def skincare_quiz(request):
     return render(request, 'quiz/skincare_Quiz.html')
 
+def skin_type_products(request, skin_type):
+    template_map = {
+        'oily': 'skincare/sub-page/oily.html',
+        'dry': 'skincare/sub-page/dry.html',
+        'combination': 'skincare/sub-page/combination.html',
+        'normal': 'skincare/sub-page/normal.html',
+        }
