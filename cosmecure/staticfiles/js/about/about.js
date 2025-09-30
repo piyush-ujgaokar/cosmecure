0076a41f7ba5b6ff603cff8f64360d96cba9d3f1
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-30 13:16:11 +0530

    Changes added for hosting

--- cosmecure/staticfiles/js/about/about.js
+++
@@ -1,8 +0,0 @@
- 
-        const mobileMenuButton = document.getElementById('mobile-menu-button');
-        const mobileMenu = document.getElementById('mobile-menu');
-
-        mobileMenuButton.addEventListener('click', () => {
-
-            mobileMenu.classList.toggle('is-open');
-        });
