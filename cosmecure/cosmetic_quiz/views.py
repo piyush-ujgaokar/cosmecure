d1c9c84c6dcf211053bf6521faea1a845090f17e
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-13 20:17:46 +0530

    buy_cosmetic view created

--- cosmecure/cosmetic_quiz/views.py
+++ cosmecure/cosmetic_quiz/views.py
@@ -7,3 +7,6 @@ def cosmetic_list(request):
     products = CosmeticProduct.objects.all()
     return render(request, 'quiz/Cosmetic_Quiz.html', {'products': products})
     
+def buy_cosmetic(request, pk):
+    product = get_object_or_404(CosmeticProduct, pk=pk)
+    return render(request, 'buy now/buy.html', {'product': product})
