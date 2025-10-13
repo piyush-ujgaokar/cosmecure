cffa9f42a7b7d2f9736e94f65193f5addde217db
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-13 20:18:58 +0530

    cosmetics url updated

--- cosmecure/cosmetic_quiz/urls.py
+++ cosmecure/cosmetic_quiz/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-path('cosmetic_quiz/', views.cosmetic_quiz, name='cosmetic_quiz'),
+path('cosmetics/', views.cosmetic_list, name='cosmetic_list'),
 ]
