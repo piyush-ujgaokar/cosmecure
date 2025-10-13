ec8fa032859290d61e49ea1610c91ce11cba00a7
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-13 20:14:30 +0530

    str obj created

--- cosmecure/cosmetic_quiz/models.py
+++ cosmecure/cosmetic_quiz/models.py
@@ -6,3 +6,6 @@ class CosmeticProduct(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
